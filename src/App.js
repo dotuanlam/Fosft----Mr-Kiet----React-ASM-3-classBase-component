42e2aa948ac93c8c93eff848999afb2999f8ee08
nhatdaithienvuong <dotuanlam681@gmail.com>
2022-05-18 10:34:04 +0700

    react ASM 3 class base component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameInput: "",
+      infor: {
+        role: null,
+        email: null,
+        company: null,
+        avatar: null,
+        followers: 0,
+      },
+    };
+  }
+  onGettingName = (e) => {
+    this.setState({
+      nameInput: (this.state.nameInput = e.target.value),
+    });
+  };
+  getData = (name) => {
+    return fetch(`https://api.github.com/users/${name}`);
+  };
+  onGettingData = () => {
+    this.getData(this.state.nameInput)
+      .then((res) => res.json())
+      .then((res) => {
+        const avatar = res.avatar_url;
+        const role = res.type;
+        const email = res.email;
+        const company = res.company;
+        const followers = res.followers;
+        this.setState({
+          infor: (this.state.infor = {
+            role: role,
+            email: email,
+            company: company,
+            avatar: avatar,
+            followers: followers,
+          }),
+        });
+      });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="content">
+            <div className="form-input">
+              <input onChange={this.onGettingName} placeholder=" " />
+              <label className="lable-input">Name</label>
+              <button className="btn-get" onClick={this.onGettingData}>
+                Get
+              </button>
+            </div>
+            <div className="UserDetailInfor">
+              <h1>User Detail Information</h1>
+            </div>
+            <div className="avatar">
+              <img alt="avatar" src={this.state.infor.avatar}></img>
+            </div>
+            <h3>
+              Role:{" "}
+              {this.state.infor.role === "null"
+                ? "Updating"
+                : this.state.infor.role}
+            </h3>
+            <h3>
+              Email:{" "}
+              {this.state.infor.email === null
+                ? "Updating"
+                : this.state.infor.email}
+            </h3>
+            <h3>Number Of Follower: {this.state.infor.followers}</h3>
+            <h3>
+              Company:{" "}
+              {this.state.infor.company === null
+                ? "Updating"
+                : this.state.infor.company}
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
